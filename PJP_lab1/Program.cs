c0f29910e85efa0c2ef637ed3132c77cb68c733e
David Tejzr <david.tejzr@seznam.cz>
2022-02-11 01:02:29 +0100

    Complete!

--- PJP_lab1/Program.cs
+++ PJP_lab1/Program.cs
@@ -4,11 +4,11 @@ namespace PJP_lab1
 {
     internal class Program
     {
-        static float sum(string first, string second)
+        static double sum(string first, string second)
         {
             try
             {
-                float res = int.Parse(first) + int.Parse(second);
+                double res = int.Parse(first) + int.Parse(second);
                 return res;
             }
             catch(Exception ex)
@@ -19,11 +19,11 @@ namespace PJP_lab1
             return -1;
         }
 
-        static float dif(string first, string second)
+        static double dif(string first, string second)
         {
             try
             {
-                float res = int.Parse(first) - int.Parse(second);
+                double res = int.Parse(first) - int.Parse(second);
                 return res;
             }
             catch (Exception ex)
@@ -34,11 +34,11 @@ namespace PJP_lab1
             return -1;
         }
 
-        static float div(string first, string second)
+        static double div(string first, string second)
         {
             try
             {
-                float res = int.Parse(first) / int.Parse(second);
+                double res = int.Parse(first) / int.Parse(second);
                 return res;
             }
             catch (Exception ex)
@@ -49,11 +49,11 @@ namespace PJP_lab1
             return -1;
         }
 
-        static float mul(string first, string second)
+        static double mul(string first, string second)
         {
             try
             {
-                float res = int.Parse(first) * int.Parse(second);
+                double res = int.Parse(first) * int.Parse(second);
                 return res;
             }
             catch (Exception ex)
@@ -64,21 +64,37 @@ namespace PJP_lab1
             return -1;
         }
 
-        static void compute(string expr)
+        static double pow(string first, string second)
         {
-            Console.WriteLine("Nacteny vyraz: {0}\n", expr);
+            try
+            {
+                double res = Math.Pow(double.Parse(first), double.Parse(second));
+                return res;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return -1;
+        }
+
+        static string compute(string expr)
+        {
+            Console.WriteLine("Expr: {0}", expr);
             string first = string.Empty;
             string second = string.Empty;
             string oper = string.Empty;
             int iter = 0;
+            int parCount = 0;
             expr = expr.Replace(" ", string.Empty);
 
             while (true)
             {
-                //read two numbers + operator
+                //nacteni dvou cisel, operatoru a kontrola dalsiho znaku
                 while (iter != expr.Length)
                 {
-                    //first number
+                    //prvni cislo
                     if (((expr[iter] >= '0') && (expr[iter] <= '9')) && (first == string.Empty))
                     {
                         while ((expr[iter] >= '0') && (expr[iter] <= '9'))
@@ -89,7 +105,7 @@ namespace PJP_lab1
                         }
                     }
 
-                    //second number
+                    //druhe cislo
                     else if (((expr[iter] >= '0') && (expr[iter] <= '9')) && (second == string.Empty))
                     {
                         while ((expr[iter] >= '0') && (expr[iter] <= '9'))
@@ -109,31 +125,62 @@ namespace PJP_lab1
                         if (iter >= expr.Length)
                         {
                             Console.WriteLine("ERROR - Unexpected end");
-                            return;
+                            return "ERROR";
+                        }
+
+                        if(expr[iter] == '*')
+                        {
+                            oper = "**";
                         }
                     }
 
-                    //next
+                    //dalsi znak
                     else if (((oper == "+") || (oper == "-")) && ((expr[iter] == '*') || (expr[iter] == '/')))
                     {
                         first = second;
                         second = string.Empty;
                         oper = expr[iter].ToString();
                         iter++;
+
+                        //hot fix pro mocninu
+                        if(expr[iter] == '*')
+                        {
+                            oper = "**";
+                        }
+
                         if (iter >= expr.Length)
                         {
                             Console.WriteLine("ERROR - Unexpected end");
-                            return;
+                            return "ERROR";
                         }
                     }
+
+                    //zacatek zavorky
+                    else if(expr[iter] == '(')
+                    {
+                        first = string.Empty;
+                        second = string.Empty;
+                        oper = string.Empty;
+
+                        parCount++;
+                        iter++;
+                    }
+
                     else
                     {
                         iter++;
                     }
                 }
 
-                //solve
-                float res = 0;
+                //kontrola konce
+                if (second == string.Empty)
+                {
+                    Console.WriteLine("Result: {0}\n", first);
+                    return first;
+                }
+
+                //reseni
+                double res = 0;
                 if (oper == "+")
                     res = sum(first, second);
                 else if (oper == "-")
@@ -142,25 +189,32 @@ namespace PJP_lab1
                     res = mul(first, second);
                 else if (oper == "/")
                     res = div(first, second);
+                else if (oper == "**")
+                    res = pow(first, second);
 
-                if (res == -1)
+                //konci kdyz vysledkem je zaporne cislo a return -1
+                if (res < 0)
                 {
                     Console.WriteLine("ERROR - Bad expression");
-                    return;
+                    return "ERROR";
                 }
 
-                Console.WriteLine("{0} {1} {2} = {3}", first, oper, second, res);
+                //Console.WriteLine("{0} {1} {2} = {3}", first, oper, second, res);
                 string resStr = first + oper + second;
 
                 //nahrazeni vyrazu vypoctem
                 expr = expr.Replace(resStr, res.ToString());
-                Console.WriteLine("New expr: {0}", expr);
 
-                if(second == string.Empty)
+                //odstraneni zavorky u vyreseneho expr
+                if ((parCount > 0) && expr.Contains("(" + res + ")"))
                 {
-                    Console.WriteLine("Result: {0}", first);
-                    return;
+                    expr = expr.Replace("(" + res.ToString() + ")", res.ToString());
+                    parCount--;
                 }
+
+                //kontrolni vyraz
+                //Console.WriteLine("New expr: {0}", expr);
+
 
                 first = string.Empty;
                 second = string.Empty;
@@ -173,16 +227,39 @@ namespace PJP_lab1
 
         static void Main(string[] args)
         {
-            string expr;
-            while (true)
+            List<string> results = new List<string>();
+            string[] exprs;
+
+            if (args.Length < 1)
             {
-                Console.WriteLine("Zadej vyraz:");
-                expr = Console.ReadLine();
-                if (expr != null)
-                {
-                    compute(expr);
-                }
+                Console.WriteLine("Input file path missing!");
+                return;
             }
+
+            try
+            {
+                exprs = File.ReadAllLines(args[0]);
+                for (int i = 1; i <= int.Parse(exprs[0].ToString()); i++)
+                {
+                    results.Add(compute(exprs[i]));
+                }
+
+                if(args.Length > 1)
+                {
+                    File.WriteAllLines(args[1], results);
+                    Console.WriteLine("\nResults saved to file {0}", args[1]);
+                }
+
+                return;
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+
+            return;
         }
     }
 }
